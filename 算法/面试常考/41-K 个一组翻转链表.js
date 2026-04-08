e1af52091ba0d58b5df0b04ab6e40a93f2ae177e
Chih-hengChen <519017928@qq.com>
2026-04-08 10:54:26 +0800

    codetop 41-45

--- 算法/面试常考/41-K 个一组翻转链表.js
+++ 算法/面试常考/41-K 个一组翻转链表.js
@@ -1,1 +1,41 @@
 // https://leetcode.cn/problems/reverse-nodes-in-k-group
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function (head, k) {
+  let cnt = 0;
+  let cur = head;
+  while (cur) {
+    cnt += 1;
+    cur = cur.next;
+  }
+
+  const dummyHead = new ListNode(null, head);
+  cur = head;
+  let prev = null;
+  let p0 = dummyHead;
+  while (cnt >= k) {
+    cnt -= k;
+
+    for (let i = 0; i < k; i++) {
+      let next = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = next;
+    }
+    const next = p0.next;
+    p0.next.next = cur;
+    p0.next = prev;
+    p0 = next;
+  }
+  return dummyHead.next;
+};
--- 算法/面试常考/44-二叉树的锯齿形层次遍历.js
+++ 算法/面试常考/44-二叉树的锯齿形层次遍历.js
@@ -1,1 +1,36 @@
 // https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+  const result = [];
+  if (!root) return result;
+  const level = [root];
+  let isEven = false;
+  while (level.length) {
+    let n = level.length;
+    const array = [];
+    while (n--) {
+      const node = level.shift();
+      array.push(node.val);
+      if (node.left) level.push(node.left);
+      if (node.right) level.push(node.right);
+    }
+    if (isEven) {
+      result.push(array.reverse());
+    } else {
+      result.push(array);
+    }
+    isEven = !isEven;
+  }
+  return result;
+};
